a878e60e9702c2f02a103e1540b253c321f1f889
Xavier Rodriguez <xrodriguez9626@arecibointer.edu>
2024-02-06 14:30:50 -0400

    Update file

--- modulo01/lab01/app.py
+++ modulo01/lab01/app.py
@@ -1,3 +1,8 @@
 print('Hola mundo!')
-mensaje = 'John Doe'
+mensaje = 'John Doe' # string value
+gpa = 3.14 # number (real) value
+isValid = True # bool value
 print(mensaje)
+print(gpa)
+print(isValid)
+print(f'{mensaje} tu GPA es {gpa:5.2}')
