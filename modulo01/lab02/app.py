996f54f0e7a3aef0b1b2c351cc57834adb2c7291
Xavier Rodriguez <xrodriguez9626@arecibointer.edu>
2024-02-08 13:52:03 -0400

    Update file

--- modulo01/lab02/app.py
+++ modulo01/lab02/app.py
@@ -2,5 +2,5 @@ mensaje = input('Entre su nombre: ')
 gpa = float(input('Entre su GPA: '))
 edad = int(input('Ingrese su edad: '))
 
-print(f' Hola {mensaje} tu GPA es {gpa}')
+print(f'Hola {mensaje} tu GPA es {gpa}')
 print(f'Tu edad es {edad}')
