59a8a750522ee439a0a7b4980d8f579e62eb4302
Xavier Rodriguez <xrodriguez9626@arecibointer.edu>
2024-02-08 14:07:33 -0400

    Update file

--- modulo01/lab03/app.py
+++ modulo01/lab03/app.py
@@ -1,7 +1,12 @@
-hourRate = float(input('Horas trabajadas'))
-payRate = float(input('Pago por hora'))
+payRate = float(input('Pago por hora: '))
+hourRate = float(input('Horas trabajadas: '))
 
 overtimeHours = 0
 
 if (hourRate > 40):
     overtimeHours = hourRate - 40
+    totalPay = (40 * payRate) + (overtimeHours * (payRate * 2))
+else:
+    totalPay = hourRate * payRate
+
+print(f'El sueldo a pagar es {totalPay}')
